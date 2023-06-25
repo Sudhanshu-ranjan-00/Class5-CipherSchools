8bcb7397b961dad2ed0100fccdb5b4e8f7238822
Sudhanshu Ranjan <rajaashujha@gmail.com>
2023-06-25 16:38:42 +0530

    Class5-CipherSchools

---
+++ primitiveDT.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class primitiveDT {
+    public static void main(String args[]) {
+        int x = 5;
+        int y = 7;
+        System.out.println("x is : " + x);
+        System.out.println("y is : " + y);
+        byte B1 = (byte) 130;
+        System.out.println("B1 is : " + B1);
+        byte b = 10;
+        System.out.println("B is : " + b);
+        int i = b;
+        System.out.println("I is : " + i);
+        float f = (float) 5.5;
+        float f1 = 10.5f;
+        double d = 32.23;
+        char ch = 'a';
+        System.out.println(ch);
+        int z = x + y;
+        System.out.println("value of z : " + z);
+
+        int m = 5;
+        int n = m++;
+        System.out.println("M is : " + m + ", N is : " + n);
+        int p = 5;
+        int q = ++p;
+        System.out.println("P is : " + p + ", Q is : " + q);
+
+    }
+
+}
